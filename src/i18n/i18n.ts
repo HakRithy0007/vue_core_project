baee28c4adccdb1f379af77718c2b127d3616957
hakrithy <hakrithykim2020@gmail.com>
2025-07-29 09:54:28 +0700

    Initial vue core project

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,16 +1,23 @@
 import { createI18n } from 'vue-i18n'
-import type { I18n } from 'vue-i18n'
+import type { I18n, LocaleMessages } from 'vue-i18n'
 import km from './km.json'
 import en from './en.json'
 
+type MessageSchema = typeof en
+
 const savedLanguage = localStorage.getItem('lang') || 'en'
-const messages = { en, km }
-const i18n = createI18n({
+
+const messages = {
+  en,
+  km,
+} as const
+
+const i18n = createI18n<[MessageSchema], 'en' | 'km'>({
   legacy: false,
   globalInjection: true,
   locale: savedLanguage,
   fallbackLocale: 'km',
   messages,
-}) as I18n
+})
 
 export default i18n
